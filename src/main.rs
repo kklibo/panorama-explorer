e2a7ab3f749c881e38845311f923beae06ffbb19
kklibo <kklibo4@gmail.com>
2021-01-19 21:44:40 -0800

    made new test program based on three-d 0.4.2 examples/texture

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,37 @@ use three_d::*;
 mod read_pto;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let screenshot_path = if args.len() > 1 { Some(args[1].clone()) } else {None};
 
-    let mut window = Window::new_default("Texture").unwrap();
+    let mut window = Window::new_default("panorama_tool").unwrap();
     let (width, height) = window.framebuffer_size();
     let gl = window.gl();
 
     // Renderer
     let mut renderer = PhongDeferredPipeline::new(&gl).unwrap();
-    let mut camera = Camera::new_perspective(&gl, vec3(4.0, 1.5, 4.0), vec3(0.0, 1.0, 0.0), vec3(0.0, 1.0, 0.0),
-                                             degrees(45.0), width as f32 / height as f32, 0.1, 1000.0);
+    let mut camera =
+        Camera::new_perspective(&gl,
+                                vec3(0.0, 0.0, 5.0),
+                                vec3(0.0, 0.0, 0.0),
+                                vec3(0.0, 1.0, 0.0),
+                                degrees(45.0),
+                                width as f32 / height as f32, 0.1, 1000.0);
 
-    Loader::load(&["examples/assets/PenguinBaseMesh.obj", "examples/assets/PenguinBaseMesh.mtl",
-        "examples/assets/penguin.png", "examples/assets/test_texture.jpg",
-        "examples/assets/skybox_evening/back.jpg", "examples/assets/skybox_evening/front.jpg",
-        "examples/assets/skybox_evening/top.jpg", "examples/assets/skybox_evening/left.jpg",
-        "examples/assets/skybox_evening/right.jpg"], move |loaded|
+    Loader::load(&["src/test_texture.jpg"], move |loaded|
     {
-        let mut box_cpu_mesh = CPUMesh {
-            positions: cube_positions(),
+
+        let mut square_cpu_mesh = CPUMesh {
+            positions: square_positions(),
             ..Default::default()
         };
-        box_cpu_mesh.compute_normals();
-        let box_material = PhongMaterial {
+        square_cpu_mesh.compute_normals();
+        let square_material = PhongMaterial {
             color_source: ColorSource::Texture(std::rc::Rc::new(texture::Texture2D::new_with_u8(&gl, Interpolation::Linear, Interpolation::Linear,
                                                                                                 Some(Interpolation::Linear), Wrapping::Repeat, Wrapping::Repeat,
-                                                                                                &Loader::get_image(loaded, "examples/assets/test_texture.jpg").unwrap()).unwrap())),
+                                                                                                &Loader::get_image(loaded, "src/test_texture.jpg").unwrap()).unwrap())),
             ..Default::default()
         };
-        let box_mesh = renderer.new_mesh(&box_cpu_mesh, &box_material).unwrap();
+        let square_mesh = renderer.new_mesh(&square_cpu_mesh, &square_material).unwrap();
 
-        let skybox = Skybox::new(&gl, &Loader::get_image(loaded, "examples/assets/skybox_evening/right.jpg").unwrap(),
-                                 &Loader::get_image(loaded, "examples/assets/skybox_evening/left.jpg").unwrap(),
-                                 &Loader::get_image(loaded, "examples/assets/skybox_evening/top.jpg").unwrap(),
-                                 &Loader::get_image(loaded, "examples/assets/skybox_evening/front.jpg").unwrap(),
-                                 &Loader::get_image(loaded, "examples/assets/skybox_evening/back.jpg").unwrap()).unwrap();
-
-        let (penguin_cpu_meshes, penguin_cpu_materials) = Obj::parse(loaded, "examples/assets/PenguinBaseMesh.obj").unwrap();
-        let penguin = renderer.new_meshes(&penguin_cpu_meshes, &penguin_cpu_materials).unwrap().remove(0);
 
         let ambient_light = AmbientLight::new(&gl, 0.4, &vec3(1.0, 1.0, 1.0)).unwrap();
         let directional_light = DirectionalLight::new(&gl, 1.0, &vec3(1.0, 1.0, 1.0), &vec3(0.0, -1.0, -1.0)).unwrap();
@@ -79,38 +71,34 @@ fn main() {
             // draw
             // Geometry pass
             renderer.geometry_pass(width, height, &|| {
-                let mut transformation = Mat4::identity();
-                box_mesh.render_geometry(&transformation, &camera)?;
-                transformation = Mat4::from_translation(vec3(-0.5, 1.0, 0.0));
-                state::cull(&gl, state::CullType::Back);
-                penguin.render_geometry(&transformation, &camera)?;
+                let transformation = Mat4::identity();
+                square_mesh.render_geometry(&transformation, &camera)?;
                 Ok(())
             }).unwrap();
 
             renderer.render_to_screen_with_forward_pass(&camera, Some(&ambient_light), &[&directional_light], &[], &[], width, height, || {
-                let transformation = Mat4::from_translation(vec3(0.5, 1.0, 0.0));
-                state::cull(&gl, state::CullType::Back);
-                penguin.mesh().render_with_ambient_and_directional(&transformation, &camera, &ambient_light, &directional_light)?;
-                skybox.apply(&camera)?;
                 Ok(())
             }).unwrap();
 
-            #[cfg(target_arch = "x86_64")]
-            if let Some(ref path) = screenshot_path {
-                let pixels = Screen::read_color(&gl, 0, 0, width, height).unwrap();
-                Saver::save_pixels(path, &pixels, width, height).unwrap();
-                std::process::exit(1);
-            }
         }).unwrap();
     });
+}
+
+fn square_positions() -> Vec<f32> {
+    vec![
+        -1.0, -1.0, 1.0,
+        1.0, -1.0, 1.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, -1.0, 1.0,
+    ]
 }
 
 
 
 
-
-
-
+/*
 fn main_old() {
 
     let mut window = Window::new_default("panorama_tool").unwrap();
@@ -184,3 +172,4 @@ fn main_old() {
     }).unwrap();
 
 }
+*/
