82a51a43239b72cab99e9d09d326032f3271004d
kklibo <kklibo4@gmail.com>
2021-02-17 16:47:12 -0800

    renamed MeshData to Photo, added convert_photo_px_to_world

--- src/main.rs
+++ src/main.rs
@@ -225,7 +225,7 @@ fn main() {
 
 
             //todo: rename this?
-            struct MeshData<'a> {
+            struct Photo<'a> {
 
                 pub mesh: &'a PhongDeferredMesh,
                 pub scale: Mat4,
@@ -233,14 +233,14 @@ fn main() {
 
             }
 
-            impl<'a> MeshData<'a> {
+            impl<'a> Photo<'a> {
 
-                pub fn from_loaded_image_mesh(m: &LoadedImageMesh) -> MeshData {
+                pub fn from_loaded_image_mesh(m: &LoadedImageMesh) -> Photo {
 
                     let scale = Mat4::from_nonuniform_scale(m.pixel_width as f32,m.pixel_height as f32,1 as f32);
                     let translate = Mat4::from_translation(Vec3::new(0f32, 0f32, 0f32));
 
-                    MeshData {
+                    Photo {
                         mesh: &m.mesh,
                         scale,
                         translate,
@@ -256,11 +256,31 @@ fn main() {
 
 
 
-            let mut mesh_datas = [
-                MeshData::from_loaded_image_mesh(&meshes[0]),
-                MeshData::from_loaded_image_mesh(&meshes[1]),
+            let mut photos = [
+                Photo::from_loaded_image_mesh(&meshes[0]),
+                Photo::from_loaded_image_mesh(&meshes[1]),
             ];
-            mesh_datas[1].translate = Mat4::from_translation(cgmath::Vector3::new(500f32, 0f32, 0f32));
+            photos[1].translate = Mat4::from_translation(cgmath::Vector3::new(500f32, 0f32, 0f32));
+
+
+
+            fn convert_photo_px_to_world(v: Vec3, m: &Photo) -> Mat4 {
+
+                let to_bottom_left = Mat4::from_translation(Vec3::new(-0.5,-0.5,0.0));
+                let to_v = Mat4::from_translation(v);
+
+                //todo: remove unwrap
+
+                //world units
+                m.translate
+                    .concat(&to_v)
+
+                    .concat(&m.scale)
+                        //scaled to photo space
+                        .concat(&to_bottom_left)
+                    .concat(&m.scale.invert().unwrap())
+
+            }
 
 
             // draw
@@ -268,7 +288,7 @@ fn main() {
             pipeline.geometry_pass(frame_input.viewport.width, frame_input.viewport.height, &|| {
 
 
-                for m in &mesh_datas {
+                for m in &photos {
 
                     m.mesh.render_geometry(RenderStates {cull: CullType::Back, ..Default::default()},
                                                    frame_input.viewport, &m.to_world(), &camera)?;
@@ -278,9 +298,9 @@ fn main() {
 
                 for &v in points {
                     let t1 = Mat4::from_nonuniform_scale(10.0,10.0,1.0);
-                    let t1 = Mat4::from_translation(Vec3::new(-0.5*meshes[0].pixel_width as f32,-0.5*meshes[0].pixel_height as f32,2f32)).concat(&t1);
-                    let t1= Mat4::from_translation(v).concat(&t1);
+                    let t1 = Mat4::from_translation(Vec3::new(0.0,0.0,1.0)).concat(&t1);
 
+                    let t1 = convert_photo_px_to_world(v, &photos[0]).concat(&t1);
                     test_mesh.render_geometry(RenderStates {cull: CullType::Back, ..Default::default()},
                                                frame_input.viewport, &t1, &camera)?;
                 }
