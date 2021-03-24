41eb6f176590fa0819c60696fd860065e2299302
kklibo <kklibo4@gmail.com>
2021-03-24 16:52:12 -0700

    added drag rotation angle indicator

--- src/render.rs
+++ src/render.rs
@@ -2,13 +2,14 @@ use three_d::definition::cpu_mesh::CPUMesh;
 use three_d::object::{Mesh, MeshProgram};
 use three_d::core::render_states::{CullType, BlendMultiplierType, BlendParameters, WriteMask, DepthTestType, RenderStates};
 use three_d::core::render_target::{Screen, ClearState};
-use three_d::math::{Vec3, Vec4, Mat4};
+use three_d::math::{Vec2, Vec3, Vec4, Mat4};
 use three_d::gui::GUI;
-use three_d::{Transform,Context,CameraControl,FrameInput,SquareMatrix};
+use three_d::{Transform,Context,CameraControl,FrameInput,SquareMatrix,InnerSpace};
 
 use crate::control_state::{ControlState, DewarpShader};
 use crate::photo::convert_photo_px_to_world;
 use crate::entities::Entities;
+use crate::viewport_geometry::{WorldCoords, ViewportGeometry};
 
 
 pub fn render(
@@ -17,6 +18,7 @@ pub fn render(
     gui: &mut GUI,
     control_state: &ControlState,
     camera: &CameraControl,
+    viewport_geometry: &ViewportGeometry,
     texture_program: &MeshProgram,
     texture_dewarp_program: &MeshProgram,
     texture_dewarp2_program: &MeshProgram,
@@ -97,12 +99,29 @@ pub fn render(
         if let Some(ref rp) = control_state.active_rotation_point {
             if let Some(ref rd) = control_state.active_rotate_drag {
 
-                //draw triangle to indicate dragged rotation angle
+                //create resized line segment for dragged rotation start line
+                let mouse_coords_vec_length =
+                    Vec2::new(
+                        rd.mouse_coords.x as f32 - rp.point.x as f32,
+                        rd.mouse_coords.y as f32 - rp.point.y as f32,
+                    ).magnitude();
 
+                let mouse_start_vec2 =
+                    Vec2::new(
+                        rd.mouse_start.x as f32 - rp.point.x as f32,
+                        rd.mouse_start.y as f32 - rp.point.y as f32,
+                    )
+                    .normalize() * mouse_coords_vec_length
+                    + Vec2::new(rp.point.x as f32, rp.point.y as f32);
+
+                let mouse_start_resized = WorldCoords{x: mouse_start_vec2.x as f64, y: mouse_start_vec2.y as f64};
+
+
+                //draw triangle to indicate dragged rotation angle
                 let cpu_mesh = CPUMesh {
                     positions: vec![
                         rp.point.x as f32, rp.point.y as f32, 0.0,
-                        rd.mouse_start.x as f32, rd.mouse_start.y as f32, 0.0,
+                        mouse_start_resized.x as f32, mouse_start_resized.y as f32, 0.0,
                         rd.mouse_coords.x as f32, rd.mouse_coords.y as f32, 0.0,
                     ],
 
@@ -115,8 +134,60 @@ pub fn render(
 
                 color_program.use_uniform_vec4("color", &Vec4::new(0.2, 0.2, 0.8, 0.5)).unwrap();
                 mesh.render(&color_program, render_states, frame_input.viewport, &t1, &camera)?;
+
+
+                //draw angle lines to indicate dragged rotation angle
+                let start_line_mat4 =   line_transform(&viewport_geometry, rp.point, mouse_start_resized,  1.0);
+                let dragged_line_mat4 = line_transform(&viewport_geometry, rp.point, rd.mouse_coords, 1.0);
+
+                color_program.use_uniform_vec4("color", &Vec4::new(0.8, 0.8, 0.2, 1.0)).unwrap();
+                entities.line_mesh.render(&color_program, render_states, frame_input.viewport, &start_line_mat4,   &camera)?;
+                entities.line_mesh.render(&color_program, render_states, frame_input.viewport, &dragged_line_mat4, &camera)?;
+
             }
         }
+
+        let p1 = WorldCoords { x: -500.0, y: 0.0 };
+        let p2 = WorldCoords { x: 50.0, y: -100.0 };
+        let pixel_thickness = 1f32;
+
+        //line render test
+        fn line_transform(
+            viewport_geometry: &ViewportGeometry,
+            p1: WorldCoords,
+            p2: WorldCoords,
+            pixel_thickness: f32,
+        ) -> Mat4
+        {
+            let p1v = Vec3::new(p1.x as f32, p1.y as f32, 0.0);
+
+            let dx = (p2.x - p1.x) as f32;
+            let dy = (p2.y - p1.y) as f32;
+
+            let line_x = Vec3::new(dx, dy, 0.0);
+
+            let mut angle = Vec3::unit_x().angle(line_x);
+            if Vec3::unit_y().dot(line_x) < 0.0 {
+                angle = -angle;
+            }
+
+            let t1 = Mat4::from_nonuniform_scale(
+                line_x.magnitude(),
+                pixel_thickness * viewport_geometry.world_units_per_pixel() as f32,
+                1.0
+            );
+            let t1 = Mat4::from_angle_z(angle).concat(&t1);
+            let t1 = Mat4::from_translation(p1v).concat(&t1);
+
+            t1
+        }
+
+        let t1 = line_transform(&viewport_geometry, p1, p2, pixel_thickness);
+
+        color_program.use_uniform_vec4("color", &Vec4::new(0.8, 0.8, 0.2, 1.0)).unwrap();
+        entities.line_mesh.render(&color_program, render_states, frame_input.viewport, &t1, &camera)?;
+
+
 
         gui.render().unwrap();
 
