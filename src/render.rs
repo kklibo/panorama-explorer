279074d2f1133b3b57a0227cd3a8629214f9167a
kklibo <kklibo4@gmail.com>
2021-05-25 16:07:12 -0700

    created primitives.rs, moved render functions to it

--- src/render.rs
+++ src/render.rs
@@ -2,18 +2,18 @@ use three_d::definition::CPUMesh;
 use three_d::object::{Mesh, MeshProgram};
 use three_d::core::{CullType, BlendMultiplierType, BlendParameters, BlendEquationType, WriteMask, DepthTestType, RenderStates};
 use three_d::core::{Screen, ClearState};
-use three_d::math::{Vec2, Vec3, Vec4, Mat4};
+use three_d::math::{Vec3, Vec4, Mat4};
 use three_d::gui::GUI;
-use three_d::{Transform, Context, CameraControl, FrameInput, SquareMatrix, InnerSpace, ColorTargetTexture2D, Camera};
-use three_d::Error;
+use three_d::{Transform, Context, CameraControl, FrameInput, SquareMatrix, ColorTargetTexture2D, Camera};
 
 use crate::control_state::ControlState;
 use crate::photo::Corner;
 use crate::entities::Entities;
-use crate::viewport_geometry::{WorldCoords, ViewportGeometry};
+use crate::ViewportGeometry;
 
 mod photo;
 mod markers;
+mod primitives;
 
 
 /// Stores immutable references used in rendering
@@ -119,64 +119,6 @@ impl Renderer<'_> {
         }).unwrap();
     }
 
-
-    fn draw_point(&self, point: WorldCoords, rotation_deg: f32, color: Vec4) -> Result<(), Error> {
-
-        let mut mesh = self.entities.color_mesh.clone();
-
-        let translate_to_point = Mat4::from_translation(Vec3::new(point.x as f32, point.y as f32, 0.0));
-        let rotate_marker = Mat4::from_angle_z(cgmath::Deg(rotation_deg));
-        let scale_marker = Mat4::from_nonuniform_scale(10.0, 10.0, 1.0);
-
-        mesh.transformation = translate_to_point * rotate_marker * scale_marker;
-
-        self.color_program.use_uniform_vec4("color", &color).unwrap();
-        mesh.render(&self.color_program, Renderer::render_states_transparency(), self.frame_input.viewport, &self.camera)
-    }
-
-    fn draw_line(
-        &self,
-        point1: WorldCoords,
-        point2: WorldCoords,
-        pixel_thickness: f32,
-        color: Vec4,
-    ) -> Result<(), Error>
-    {
-        let mut mesh =  self.entities.line_mesh.clone();
-
-        mesh.transformation = Renderer::line_transform(&self.viewport_geometry, point1, point2, pixel_thickness);
-
-        self.color_program.use_uniform_vec4("color", &color).unwrap();
-        mesh.render(&self.color_program, Renderer::render_states_transparency(), self.frame_input.viewport, &self.camera)
-    }
-
-    fn line_transform(
-        viewport_geometry: &ViewportGeometry,
-        p1: WorldCoords,
-        p2: WorldCoords,
-        pixel_thickness: f32,
-    ) -> Mat4
-    {
-        let p1v = Vec3::new(p1.x as f32, p1.y as f32, 0.0);
-
-        let dx = (p2.x - p1.x) as f32;
-        let dy = (p2.y - p1.y) as f32;
-
-        let line_x = Vec2::new(dx, dy);
-
-        let angle = Vec2::unit_x().angle(line_x);
-
-        let translate_to_p1 = Mat4::from_translation(p1v);
-        let rotate_around_p1 = Mat4::from_angle_z(angle);
-        let scale_length_and_thickness =
-            Mat4::from_nonuniform_scale(
-                line_x.magnitude(),
-                pixel_thickness * viewport_geometry.world_units_per_pixel() as f32,
-                1.0
-            );
-
-        translate_to_p1 * rotate_around_p1 * scale_length_and_thickness
-    }
 
     fn render_map_overlay(&self) {
 
