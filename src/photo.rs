072b2ae3b4f199c70e66db2e0c36cc10ef5ab342
kklibo <kklibo4@gmail.com>
2021-06-04 17:11:31 -0700

    added Photo::world_coords + tests
    new tests are redundant with existing WorldRectangle::convert_local_to_world tests

--- src/photo.rs
+++ src/photo.rs
@@ -1,13 +1,13 @@
 use std::rc::Rc;
 use std::fmt::{Display,Formatter};
 
-use three_d::{Mat4,Texture};
+use three_d::{Vec3,Vec4,Mat4,Texture};
 
 use serde::{Serialize, Deserialize, Serializer};
 use serde::ser::SerializeStruct;
 
 pub use crate::entities::LoadedImageMesh;
-use crate::viewport_geometry::WorldCoords;
+use crate::viewport_geometry::{WorldCoords, PixelCoords};
 use crate::world_rectangle::WorldRectangle;
 
 
@@ -46,11 +46,11 @@ impl Display for Photo {
 
 impl Photo {
 
-    pub fn from_loaded_image_mesh(m: Rc<LoadedImageMesh>) -> Photo {
+    pub fn from_loaded_image_mesh(m: Rc<LoadedImageMesh>) -> Self {
 
         let orientation = WorldRectangle::new(m.texture_2d.width() as f32,m.texture_2d.height() as f32);
 
-        Photo {
+        Self {
             loaded_image_mesh: m,
             orientation,
         }
@@ -88,5 +88,81 @@ impl Photo {
     pub fn rotate_around_point(&mut self, angle: f32, point: WorldCoords) {
 
         self.orientation.rotate_around_point(angle, point)
+    }
+
+    /// gets the WorldCoords location of pixel coords in this photo
+    pub fn world_coords(&self, pixel_coords: PixelCoords) -> WorldCoords {
+
+        Self::world_coords_impl(&self.orientation, pixel_coords)
+    }
+
+    fn world_coords_impl(world_rectangle: &WorldRectangle, pixel_coords: PixelCoords) -> WorldCoords {
+
+        let v = Vec3::new(pixel_coords.x as f32, pixel_coords.y as f32, 0.0);
+        let m = world_rectangle.convert_local_to_world(v);
+
+        let out_v = m * Vec4::unit_w();
+
+        WorldCoords{x: out_v.x as f64, y: out_v.y as f64}
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+
+    #[test]
+    fn world_coords_test() {
+
+        //at origin, no rotation
+        {
+            let mut orientation = WorldRectangle::new(200.0, 100.0);
+            orientation.set_rotation(0.0);
+            orientation.set_translation(WorldCoords { x: 0.0, y: 0.0 });
+
+            //top left corner
+            {
+                let pixel_coords = PixelCoords { x: 0.0, y: 0.0 };
+                let world_coords = Photo::world_coords_impl(&orientation, pixel_coords);
+
+                assert_eq!(world_coords, WorldCoords { x: -100.0, y: 50.0 });
+            }
+
+            //bottom right corner
+            {
+                let pixel_coords = PixelCoords { x: 200.0, y: 100.0 };
+                let world_coords = Photo::world_coords_impl(&orientation, pixel_coords);
+
+                assert_eq!(world_coords, WorldCoords { x: 100.0, y: -50.0 });
+            }
+        }
+
+        //rotated + translated
+        {
+            let mut orientation = WorldRectangle::new(200.0, 100.0);
+            orientation.set_rotation(90.0);
+            let x = 2000.0;
+            let y = 1000.0;
+            orientation.set_translation(WorldCoords {x,y});
+
+            //top left corner
+            {
+                let pixel_coords = PixelCoords { x: 0.0, y: 0.0 };
+                let world_coords = Photo::world_coords_impl(&orientation, pixel_coords);
+
+                assert_eq!(world_coords, WorldCoords { x: x - 50.0, y: y - 100.0 });
+            }
+
+            //bottom right corner
+            {
+                let pixel_coords = PixelCoords { x: 200.0, y: 100.0 };
+                let world_coords = Photo::world_coords_impl(&orientation, pixel_coords);
+
+                assert_eq!(world_coords, WorldCoords { x: x + 50.0, y: y + 100.0 });
+            }
+        }
+
     }
 }
